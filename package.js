a68f812bd3be14cf6ee30788642460b355cdc10a
Morten Henriksen <mh@gi2.dk>
2014-12-17 20:46:54 +0100

    bump versions

--- package.js
+++ package.js
@@ -10,7 +10,7 @@ Package.onUse(function (api) {
   if (api.versionsFrom) {
     api.versionsFrom('1.0');
 
-    api.use('ground:store@0.1.0');
+    api.use('ground:store@0.1.1');
 
     // Make sure any storage adapters are loaded first
     api.use([
