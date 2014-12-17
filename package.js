cb48fa27dcefe6fb4e08ff163d77611ddadf6653
Morten Henriksen <mh@gi2.dk>
2014-12-17 15:54:24 +0100

    Bump to version 0.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "ground:servertime",
-  version: "0.0.1",
+  version: "0.0.2",
   summary: "Adds server time on the client",
   git: "https://github.com/GroundMeteor/servertime.git"  
 });
